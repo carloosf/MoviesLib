55a7d6cabd7220959540cf07f6ffbc9cc39ecca3
Carlos Silva <silvaugusto222@gmail.com>
2023-03-10 00:59:53 -0300

    configurando navbar.jsx e css

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,15 +1,14 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <Link to="/">MoviesLib</Link>
-      </nav>
-      <Link to="/movie/1">Movie</Link>
-      <Link to="/search">Search</Link>
-
+      <Navbar>
+        
+      </Navbar>
       <h2>Movies Lib</h2>
       <Outlet />
     </div>
