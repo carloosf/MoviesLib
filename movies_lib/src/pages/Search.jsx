076fcc47a0b41914ed6444e19a7436a04a10997d
Carlos Silva <silvaugusto222@gmail.com>
2023-03-11 00:28:39 -0300

    Correção do .env

--- movies_lib/src/pages/Search.jsx
+++ movies_lib/src/pages/Search.jsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom"
 import MovieCard from "../components/MovieCard"
 
 const searchURL = import.meta.env.VITE_SEARCH
-const apiKey = import.meta.env.VITE_API_KEY
+const apiKey = import.meta.env.VITE_API_KEY;
 
 import "./MoviesGrid.css"
 
